efa1e1235c6abcfae70e274359b4783a8099dafc
Alexandre Boutrik <alexandreboutrik@protonmail.ch>
2026-05-08 15:21:43 +0200

    chore(nix): add optional GnuPG 2.5 compilation for Kyber support
    - Add `useCustomGnuPG` CLI argument to `shell.nix` to optionally compile GnuPG
      2.5.19, libgcrypt 1.12.0, and libgpg-error 1.56 from source. This is needed
      for KYBER.
    - Configure an isolated GNUPGHOME directory within the project to prevent host
      system agent interference.
    - Ignore the local `.gnupg-bouclier/` directory in `.gitignore`.
    - Add exported public key (LibrePGP v5) to repository assets.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,38 @@
+{ useCustomGnuPG ? false }:
+
 let
   rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+
+  gnupg_overlay = final: prev: {
+	libgpg-error = prev.libgpg-error.overrideAttrs (old: rec {
+      version = "1.56";
+      src = prev.fetchurl {
+        url = "https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-${version}.tar.bz2";
+        hash = "sha256-gsPS3rStlq05Jdb58ST+cgVxYFWrUOKREW7yeXXRacA=";
+      };
+    });
+
+    libgcrypt = prev.libgcrypt.overrideAttrs (old: rec {
+      version = "1.12.0";
+      src = prev.fetchurl {
+        url = "https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-${version}.tar.bz2";
+        hash = "sha256-AxFFTmeBibrWKn6UAqndeTAl7/9udEmJhhbi/HXg9PU=";
+      };
+    });
+
+    gnupg = prev.gnupg.overrideAttrs (old: rec {
+      version = "2.5.19";
+      src = prev.fetchurl {
+        url = "https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-${version}.tar.bz2";
+        hash = "sha256-ciqopCbdm0Tg0ZS3O/7jo+YX1lZ0zU0dBi5t8p8XiMY=";
+      };
+	  patches = [];
+    });
+  };
   
   pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+
+  gnupg_pkgs = import <nixpkgs> { overlays = [ gnupg_overlay ]; };
 
   rust-toolchain = pkgs.rust-bin.stable.latest.default.override {
     extensions = [ "rust-src" "rustfmt" "clippy" ];
@@ -22,7 +53,7 @@ pkgs.mkShell {
     llvm
     pkg-config
 	incus
-  ];
+  ] ++ (if useCustomGnuPG then [ gnupg_pkgs.gnupg ] else [ gnupg ]);
 
   buildInputs = with pkgs; [
     libbpf
@@ -56,10 +87,17 @@ pkgs.mkShell {
     export BPF_CLANG="${pkgs.llvmPackages.clang-unwrapped}/bin/clang"
     export BPF_CFLAGS="-I${pkgs.libbpf}/include -I${pkgs.linuxHeaders}/include"
 
+    # Gnupg
+    export GNUPGHOME=$PWD/.gnupg-bouclier
+    mkdir -p $GNUPGHOME ; chmod 700 $GNUPGHOME
+    gpgconf --kill all
+    gpg-agent --daemon
+
     echo "* Welcome to the bouclier-bleu NixOS dev shell!"
     echo "------------------------------------------------"
     echo "Rust version:  $(rustc --version)"
     echo "Clang version: $(clang --version | head -n 1)"
+    echo "Gnupg version: $(gpg --version | head -n 1)"
     echo "------------------------------------------------"
     echo "Run: cargo build"
   '';
