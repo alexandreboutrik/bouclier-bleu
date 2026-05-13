ec0bdd27e9117cf20ac5de982a777b7269d4ae2c
Alexandre Boutrik <alexandreboutrik@protonmail.ch>
2026-05-13 02:54:28 +0200

    chore(release): prepare v0.11.1

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         in {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "bouclier-bleu";
-            version = "0.11.0";
+            version = "0.11.1";
 
             src = ./.;
 
